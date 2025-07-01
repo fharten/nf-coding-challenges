d2182c3faa89e48b69daf38cd320761e7321bbe8
fharten <florian.harten@pm.me>
2025-07-01 12:00:10 +0200

    added a bit of styling to network test

--- 250701/network/app/page.tsx
+++ 250701/network/app/page.tsx
@@ -47,7 +47,6 @@ export default function Home() {
   const url = 'https://jsonplaceholder.typicode.com/posts';
   const urlUsers = 'https://jsonplaceholder.typicode.com/users';
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const getPosts = async (): Promise<void> => {
     const res = await fetch(url);
 
@@ -108,90 +107,115 @@ export default function Home() {
 
   useEffect(() => {
     getUsers();
-    //getPosts()
+    getPosts();
     // getPostsWrong();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   //
   return (
-    <div>
+    <div className='flex flex-col w-3xl mt-24 mx-auto'>
       {error && <p id='errorMsg'>{error}</p>}
 
       <div>
-        <h1>new Post</h1>
-        <form>
+        <h1 className='text-center font-bold text-2xl'>Add new post</h1>
+        <form className='flex gap-x-2'>
           <input
+            className='border rounded-sm px-2'
             type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             placeholder='title...'
           />
           <input
+            className='border rounded-sm px-2'
             type='text'
             value={body}
             onChange={(e) => setBody(e.target.value)}
             placeholder='body...'
           />
-          <button type='button' onClick={newPost}>
+          <button
+            className={`px-4 border rounded-sm bg-gray-100 text-gray-900 hover:cursor-pointer hover:invert`}
+            type='button'
+            onClick={newPost}
+          >
             add post
           </button>
         </form>
       </div>
 
-      <div>
-        <h1>new User</h1>
-        <form>
+      <div className='my-18'>
+        <h1 className='text-center font-bold text-2xl'>Add new user</h1>
+        <form className='flex gap-x-2'>
           <input
+            className='border rounded-sm px-2'
             type='text'
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             placeholder='username...'
           />
           <input
+            className='border rounded-sm px-2'
             type='text'
             value={name}
             onChange={(e) => setName(e.target.value)}
             placeholder='name...'
           />
           <input
+            className='border rounded-sm px-2'
             type='text'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder='email...'
           />
-          <button type='button' onClick={newUser}>
+          <button
+            className={`px-4 border rounded-sm bg-gray-100 text-gray-900 hover:cursor-pointer hover:invert`}
+            type='button'
+            onClick={newUser}
+          >
             add user
           </button>
         </form>
       </div>
-
-      {post && (
+      <div className='flex'>
+        <div className='flex-1'>
+          {post && (
+            <div className='mb-12'>
+              <h2 className='font-bold text-xl'>New post</h2>
+              <p>{post.id}</p>
+            </div>
+          )}
+        </div>
         <div>
-          <p>{post.id}</p>
+          {user && (
+            <div className='mb-12'>
+              <h2 className='font-bold text-xl min-w-48'>New User</h2>
+              <p>{user.id}</p>
+              <p>{user.name}</p>
+              <p>{user.username}</p>
+              <p>{user.email}</p>
+            </div>
+          )}
         </div>
-      )}
-
-      {user && (
+      </div>
+      <div className='flex'>
+        <div className='flex-1'>
+          <h2 className='font-bold text-xl'>Post titles</h2>
+          {posts.map((post, i) => (
+            <div key={i}>
+              <ul id='postList'>{post.title}</ul>
+            </div>
+          ))}
+        </div>
         <div>
-          <p>{user.id}</p>
-          <p>{user.name}</p>
-          <p>{user.username}</p>
-          <p>{user.email}</p>
+          <h2 className='font-bold text-xl min-w-48'>Usernames</h2>
+          {users.map((user, i) => (
+            <div key={i}>
+              <ul id='userList'>{user.name}</ul>
+            </div>
+          ))}
         </div>
-      )}
-
-      {posts.map((post, i) => (
-        <div key={i}>
-          <ul id='postList'>{post.title}</ul>
-        </div>
-      ))}
-
-      {users.map((user, i) => (
-        <div key={i}>
-          <ul id='userList'>{user.name}</ul>
-        </div>
-      ))}
+      </div>
     </div>
   );
 }

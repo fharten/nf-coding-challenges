c5314a1b560e8e6f68ec5bb9901fb9258658b6cc
fharten <florian.harten@pm.me>
2025-07-02 11:09:43 +0200

    tdd - added 2 more tests for greeting

--- 250702/src/utils/greet.test.ts
+++ 250702/src/utils/greet.test.ts
@@ -3,3 +3,11 @@ import { greet } from './greet';
 test('returns greeting with given name', () => {
   expect(greet('Lisa')).toBe('Hello, Lisa!');
 });
+
+test('returns empty string', () => {
+  expect(greet('')).toBe('Hello, !');
+});
+
+test('returns greeting the world', () => {
+  expect(greet('world')).toBe('Hello, world!');
+});

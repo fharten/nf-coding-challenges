4018751918590718b7dcaa3f871fd88b2c293c75
fharten <florian.harten@pm.me>
2025-07-02 11:24:18 +0200

    tdd - added 3 capitalize tests

--- 250702/src/utils/capitalize.test.ts
+++ 250702/src/utils/capitalize.test.ts
@@ -3,3 +3,15 @@ import { capitalize } from './capitalize';
 test('capitalizes a single lowercase word', () => {
   expect(capitalize('hello')).toBe('Hello');
 });
+
+test('capitalizes a single lowercase word', () => {
+  expect(capitalize('h')).toBe('H');
+});
+
+test('capitalizes a single lowercase word', () => {
+  expect(capitalize('')).toBe('');
+});
+
+test('capitalizes a single lowercase word', () => {
+  expect(capitalize('javaScript')).toBe('JavaScript');
+});

4c2b9a73cfdb5a3ed43e126f47c121d02c8d9afd
fharten <florian.harten@pm.me>
2025-07-02 11:29:09 +0200

    tdd - added 3 isPalindrome tests

--- 250702/src/utils/palindrome.test.ts
+++ 250702/src/utils/palindrome.test.ts
@@ -3,3 +3,15 @@ import { isPalindrome } from './palindrome';
 test('noon is a palindrome', () => {
   expect(isPalindrome('noon')).toBe(true);
 });
+
+test('test is no palindrome', () => {
+  expect(isPalindrome('test')).toBe(false);
+});
+
+test('madam is a palindrome', () => {
+  expect(isPalindrome('madam')).toBe(true);
+});
+
+test(' is a palindrome', () => {
+  expect(isPalindrome('')).toBe(true);
+});

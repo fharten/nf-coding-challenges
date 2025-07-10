d17e287491f03a43ab7e9fcc7062ae6795f7d07b
fharten <florian.harten@pm.me>
2025-07-10 09:13:42 +0200

    named .prettierrc correctly

--- 250709/blog/src/app.ts
+++ 250709/blog/src/app.ts
@@ -1,19 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 
-import express from 'express';
-import type { Request, Response } from 'express';
-import nunjucks from 'nunjucks';
-import cors from 'cors';
-import slugify from 'slugify';
-import blogData from '../data/blogentries.json';
+import express from "express";
+import type { Request, Response } from "express";
+import nunjucks from "nunjucks";
+import cors from "cors";
+import slugify from "slugify";
+import blogData from "../data/blogentries.json";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-nunjucks.configure('src/templates', {
+nunjucks.configure("src/templates", {
   autoescape: true,
   express: app,
 });
@@ -21,34 +21,34 @@ nunjucks.configure('src/templates', {
 const newBlogData = blogData.map((p) => ({
   ...p,
   slug: slugify(p.title, { remove: /[*+~.,()'"!:@]/g }),
-  date: new Date(p.createdAt * 1000).toLocaleDateString('de-DE'),
+  date: new Date(p.createdAt * 1000).toLocaleDateString("de-DE"),
 }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.render('index.html', { newBlogData });
+app.get("/", (req: Request, res: Response) => {
+  res.render("index.html", { newBlogData });
 });
 
-app.get('/index.html', (req: Request, res: Response) => {
-  res.render('index.html', { newBlogData });
+app.get("/index.html", (req: Request, res: Response) => {
+  res.render("index.html", { newBlogData });
 });
 
-app.get('/about.html', (req: Request, res: Response) => {
-  res.render('about.html');
+app.get("/about.html", (req: Request, res: Response) => {
+  res.render("about.html");
 });
 
-app.get('/contact.html', (req: Request, res: Response) => {
-  res.render('contact.html');
+app.get("/contact.html", (req: Request, res: Response) => {
+  res.render("contact.html");
 });
 
-app.get('/post/:slug', (req: Request, res: Response) => {
+app.get("/post/:slug", (req: Request, res: Response) => {
   const slug = req.params.slug;
   const post = newBlogData.find((p) => p.slug === slug);
 
   if (!post) {
-    return res.status(404).send('Post not found');
+    return res.status(404).send("Post not found");
   }
 
-  res.render('post.html', {
+  res.render("post.html", {
     title: post.title,
     image: post.image,
     author: post.author,
@@ -58,14 +58,14 @@ app.get('/post/:slug', (req: Request, res: Response) => {
   });
 });
 
-app.get('/post.html', (req: Request, res: Response) => {
+app.get("/post.html", (req: Request, res: Response) => {
   const post = newBlogData[0];
 
   if (!post) {
-    return res.status(404).send('Post not found');
+    return res.status(404).send("Post not found");
   }
 
-  res.render('post.html', {
+  res.render("post.html", {
     title: post.title,
     image: post.image,
     author: post.author,

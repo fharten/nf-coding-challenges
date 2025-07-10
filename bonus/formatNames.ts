7261aa360f36ea86b7f117eabfe2479da1a09862
fharten <florian.harten@pm.me>
2025-07-10 16:59:33 +0200

    added bonus exercise Format a List of Name

---
+++ bonus/formatNames.ts
@@ -0,0 +1,15 @@
+type Name = { name: string };
+
+const formatNames = (names: Name[]): string => {
+  if (names.length < 1) return '';
+  if (names.length === 1) return names[0].name;
+  if (names.length === 2) return `${names[0].name} & ${names[1].name}`;
+
+  const nameMap = names.map((i) => i.name);
+
+  return `${nameMap.slice(0, -1).join(', ')} & ${nameMap[names.length - 1]}`;
+};
+
+const list = [{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' }];
+
+console.log(formatNames(list));

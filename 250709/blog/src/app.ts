21fba246f206a7d1927633386cd5a258dcc8566c
fharten <florian.harten@pm.me>
2025-07-10 08:43:04 +0200

    added nunjucks blog

---
+++ 250709/blog/src/app.ts
@@ -0,0 +1,80 @@
+require('dotenv').config();
+
+import express from 'express';
+import type { Request, Response } from 'express';
+import nunjucks from 'nunjucks';
+import cors from 'cors';
+import slugify from 'slugify';
+import blogData from '../data/blogentries.json';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static('public'));
+
+nunjucks.configure('src/templates', {
+  autoescape: true,
+  express: app,
+});
+
+const newBlogData = blogData.map((p) => ({
+  ...p,
+  slug: slugify(p.title, { remove: /[*+~.,()'"!:@]/g }),
+  date: new Date(p.createdAt * 1000).toLocaleDateString('de-DE'),
+}));
+
+app.get('/', (req: Request, res: Response) => {
+  res.render('index.html', { newBlogData });
+});
+
+app.get('/index.html', (req: Request, res: Response) => {
+  res.render('index.html', { newBlogData });
+});
+
+app.get('/about.html', (req: Request, res: Response) => {
+  res.render('about.html');
+});
+
+app.get('/contact.html', (req: Request, res: Response) => {
+  res.render('contact.html');
+});
+
+app.get('/post/:slug', (req: Request, res: Response) => {
+  const slug = req.params.slug;
+  const post = newBlogData.find((p) => p.slug === slug);
+
+  if (!post) {
+    return res.status(404).send('Post not found');
+  }
+
+  res.render('post.html', {
+    title: post.title,
+    image: post.image,
+    author: post.author,
+    date: post.date,
+    teaser: post.teaser,
+    content: post.content,
+  });
+});
+
+app.get('/post.html', (req: Request, res: Response) => {
+  const post = newBlogData[0];
+
+  if (!post) {
+    return res.status(404).send('Post not found');
+  }
+
+  res.render('post.html', {
+    title: post.title,
+    image: post.image,
+    author: post.author,
+    date: post.date,
+    teaser: post.teaser,
+    content: post.content,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ 250709/nunjucks-example/src/app.ts
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+import express from 'express';
+import type { Request, Response } from 'express';
+import nunjucks from 'nunjucks';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+nunjucks.configure('src/templates', {
+  autoescape: true,
+  express: app,
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.render('home.html', {
+    title: 'Nunjucks Example',
+  });
+});
+
+app.get('/about', (req: Request, res: Response) => {
+  res.render('about.html', {
+    title: 'About page',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

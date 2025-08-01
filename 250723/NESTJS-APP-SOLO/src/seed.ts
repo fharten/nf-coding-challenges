d47271c779ddbfc43948a6eaf2a56c505229b687
fharten <florian.harten@pm.me>
2025-08-01 13:45:12 +0200

    added dockerfile to NEST-JS-APP-SOLO

--- 250723/NESTJS-APP-SOLO/src/seed.ts
+++ 250723/NESTJS-APP-SOLO/src/seed.ts
@@ -1,11 +1,10 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { QuotesService } from "./quotes/quotes.service";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { QuotesService } from './quotes/quotes.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const quotesService = app.get(QuotesService);
-  await quotesService.saveQuotesFromJson(); // Methode wie oben beschrieben
   await app.close();
 }
 

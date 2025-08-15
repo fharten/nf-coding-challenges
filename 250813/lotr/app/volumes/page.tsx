49d63dbe357c2f9400258ee87a9130928eb0c616
fharten <florian.harten@pm.me>
2025-08-15 12:25:19 +0200

    swapped random button for a link to use less code and dont need a client component

--- 250813/lotr/app/volumes/page.tsx
+++ 250813/lotr/app/volumes/page.tsx
@@ -1,20 +1,8 @@
-'use client';
-
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { volumes, introduction } from '@/data/data';
 
 export default function VolumesPage() {
-  const router = useRouter();
-
-  const handleRandomVolume = () => {
-    if (volumes.length === 0) return;
-
-    const randomIndex = Math.floor(Math.random() * volumes.length);
-    const randomVolume = volumes[randomIndex];
-
-    router?.push(`/volumes/${randomVolume.slug}`);
-  };
+  const randomIndex = Math.floor(Math.random() * volumes.length);
 
   return (
     <div className='flex flex-col max-w-4xl mx-auto gap-y-10 items-center justify-center mt-40'>
@@ -29,12 +17,12 @@ export default function VolumesPage() {
         ))}
       </ul>
       <div className='text-center mt-20'>
-        <button
-          onClick={handleRandomVolume}
+        <Link
+          href={`/volumes/${volumes[randomIndex].slug}`}
           className='px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition-colors'
         >
           Visit Random Volume
-        </button>
+        </Link>
       </div>
     </div>
   );
